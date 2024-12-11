a092d34095db2683b604a160cd8c54b6abf5127c
Saidul147 <sidul.islam.146@gmail.com>
2024-12-11 09:49:40 +0600

    add said

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import Footer from './components/footer/Footer'
 import FooterHeader from './components/footer/FooterHeader'
-import FormSchedule from './components/Form-schedule/FormSchedule'
+
 import ClientTestimonials from './components/client-testimonials/ClientTestimonials'
 import CaseStudies from './components/Case-studies/CaseStudies'
 
